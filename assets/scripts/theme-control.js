98940da3227284f0977e3bade0bb8ce9f4cf4bd6
thewildmage <cecilia@hey.com>
2022-02-17 18:43:40 -0700

    theme-control: add initialize, consolidate redundancies

--- assets/scripts/theme-control.js
+++ assets/scripts/theme-control.js
@@ -1,34 +1,40 @@
 let parent = document.getElementById("js-theme-control");
 let controls = parent.querySelectorAll("button");
-
-controls.forEach((control) => {
-    control.addEventListener("click", (event) => {
-        changeTheme(event.target.value);
-    });
-});
-
 let storageKey = "theme";
-let choice = localStorage.getItem(storageKey);
 
-if (choice) {
+export function initialize() {
     controls.forEach((control) => {
-        if (control.value === choice) {
-            unpressAll();
-            control.setAttribute("aria-pressed", "true");
-
-            changeTheme(control.value);
-        }
+        control.addEventListener("click", (event) => {
+            changeTheme(event.target);
+        });
     });
+
+    let choice = localStorage.getItem(storageKey);
+
+    if (choice) {
+        controls.forEach((control) => {
+            if (control.value === choice) {
+                changeTheme(control);
+            }
+        });
+    }
 }
 
 function unpressAll() {
     controls.forEach((control) => {
-        control.setAttribute("aria-pressed", "false");
+        toggle(control, false);
     });
 }
 
-function changeTheme(theme) {
-    localStorage.setItem(storageKey, theme);
+function toggle(control, isPressed) {
+    control.setAttribute("aria-pressed", isPressed);
+}
 
+function changeTheme(control) {
+    let theme = control.value;
+
+    unpressAll();
+    toggle(control, true);
+    localStorage.setItem(storageKey, theme);
     document.body.dataset[storageKey] = theme;
 }
