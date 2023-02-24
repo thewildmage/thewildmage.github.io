e4f7fee6c983fd8253b635775c07d4f281997ba2
thewildmage <cecilia@hey.com>
2023-02-24 16:13:05 -0700

    Use Lume to handle all source files

--- _config.js
+++ _config.js
@@ -1,5 +1,15 @@
 import lume from "lume/mod.ts";
 
-const site = lume();
+const site = lume({
+	dest: "./_distribution",
+	src: "./source"
+});
+
+site.copy("favicon.ico")
+	.copy("manifest.webmanifest")
+	.copy([".png", ".svg"])
+	.loadPages([".html"])
+	.loadAssets([".css", ".js"])
+;
 
 export default site;
