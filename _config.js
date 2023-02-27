889ef8a269db02cca1b3a2e1c9d90e4346c6604b
thewildmage <cecilia@hey.com>
2023-02-26 19:36:04 -0700

    Wire up Lume plugins

--- _config.js
+++ _config.js
@@ -1,5 +1,8 @@
 import lume from "lume/mod.ts";
 
+import anchorHeadings from "plugins/anchor_headings.ts";
+import { formatWithPrettier } from "plugins/format.js";
+
 const site = lume({
 	dest: "./_distribution",
 	src: "./source"
@@ -10,6 +13,8 @@ site.copy("favicon.ico")
 	.copy([".png", ".svg"])
 	.loadPages([".html"])
 	.loadAssets([".css", ".js"])
+	.use(anchorHeadings)
+	.process([".css", ".html", ".js"], formatWithPrettier)
 ;
 
 export default site;
